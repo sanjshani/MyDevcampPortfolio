436ef2a8f46ac2adea3fe59017c43093634958c1
Sanjaya Ranaweera <sanjshani@yahoo.com>
2019-06-28 01:32:10 -0400

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,14 +9,33 @@ class PortfoliosController < ApplicationController
 
 	def create
 	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
+# These are the parameters that's allow to go into the system >> i.e. what we type with new method!!!
+# Additionally, we need to change the image tag 
 	    respond_to do |format|
 	    	if @portfolio_item.save
 	    		format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+# Changed from redirect_to >> portfolio_item (let go back to enter another item ) to, >> portfolio_path (go back to index?? page)
+
 	        else
 	        	format.html { render :new }
 	        end
 	    end
 	end
-end
 
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+
+		respond_to do |format|
+		  if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+		    format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+		  else
+		    format.html { render :edit }
+		  end
+		end
+	end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 10.times do |blog|
 	Blog.create!(
-		title: "My Blog Post #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+		title: "My Blog Post #{blog}",				# This line creates a new id#, everytime it iterates through the loop!!!
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	)
 end
 
@@ -17,7 +9,7 @@ puts "10 blog posts created"
 
 5.times do |skill|
 	Skill.create!(
-		title: "Rails #{skill}",
+		title: "Rails #{skill}",					# This line creates a new id#, everytime it iterates through the loop!!!
 		percent_utilized: 15
 	)
 end
@@ -26,7 +18,7 @@ puts "5 skills created"
 
 9.times do |portfolio_item|
 	Portfolio.create!(
-		title: "Portfolio title #{portfolio_item}",
+		title: "Portfolio title #{portfolio_item}",	# This line creates a new id#, everytime it iterates through the loop!!!
 		subtitle: "My great service",
 		body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		main_image: "https://via.placeholder.com/600x400",
@@ -35,3 +27,11 @@ puts "5 skills created"
 end
 
 puts "9 portfolio items created"
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
