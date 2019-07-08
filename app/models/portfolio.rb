3faf3f5701922b86ab6357f27411d09d1977eb2b
Sanjaya Ranaweera <sanjshani@yahoo.com>
2019-07-07 21:02:19 -0400

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,13 @@ class Portfolio < ApplicationRecord
 		where(subtitle: 'Angular')
 	end
 
-	# Scope
+	# Scope Method to run db queries << had not yet created a route and a method to use in browser!!
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/300x200"
+	end
 end
