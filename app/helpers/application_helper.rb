de8e6ce29596d351a51b49362f91bd5a4c327e5e
Sanjaya Ranaweera <sanjshani@yahoo.com>
2020-02-11 14:10:39 -0500

    Refactored alerts to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,6 +57,18 @@ module ApplicationHelper
   def active? path
   	"active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert]) || (flash[:error]) || (flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "L. Sanjaya Ranaweera's Portfolio", sticky: false)
+  end
 end
 
 
